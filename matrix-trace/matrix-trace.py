ade268001f68591981ade220dcb1b2a54bf630be
Đỗ Duy Thành <84120300+duythanh22@users.noreply.github.com>
2026-03-07 21:26:00 +0700

    Update solution for matrix-trace

--- matrix-trace/matrix-trace.py
+++ matrix-trace/matrix-trace.py
@@ -6,8 +6,6 @@ def matrix_trace(A):
     """
     sum = 0
     for i in range(len(A)):
-        for j in range(len(A)):
-           if i == j:
-               sum += A[i][j]
+        sum += A[i][i]
     return sum
     
