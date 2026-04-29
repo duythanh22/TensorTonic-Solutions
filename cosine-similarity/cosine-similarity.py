02efea3359eceea1b62ac0e91ff909e1dc324db3
Đỗ Duy Thành <84120300+duythanh22@users.noreply.github.com>
2026-04-29 16:48:40 +0700

    Update solution for cosine-similarity

--- cosine-similarity/cosine-similarity.py
+++ cosine-similarity/cosine-similarity.py
@@ -1,22 +1,24 @@
 import numpy as np
 
+
 def cosine_similarity(a, b):
     """
     Compute cosine similarity between two 1D NumPy arrays.
     Returns: float in [-1, 1]
     """
-
-    # step 1
+    # step 1: cal dot a, b
     a = np.asarray(a)
     b = np.asarray(b)
-
     dot_ab = np.dot(a, b)
-
-    # step 2
-    norm2_a = np.linalg.norm(a, ord=2)
-    norm2_b = np.linalg.norm(b, ord=2)
     
+    # step 2: cal L2
+    norm2_a = np.linalg.norm(a)
+    norm2_b = np.linalg.norm(b)
+    
+    # step 3: cal sim (validate when norm = 0)
     if norm2_a == 0 or norm2_b == 0:
         return 0.0
-    # step 3
-    return (dot_ab / (norm2_a*norm2_b)).astype(np.float32)
+    
+    return float(dot_ab / (norm2_a*norm2_b))
+
+print(cosine_similarity([1,2,3], [2,4,6]))
